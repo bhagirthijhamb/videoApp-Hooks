a064c8a6604748813b83c430000abd1f3579498b
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-17 15:13:44 -0500

    added styles

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import VideoList from './VideoList';
 import './VideoItem.css';
 
 const VideoItem = ({ video , onVideoSelect}) => {
   return (
     <div onClick={() => onVideoSelect(video)} className="video-item item">
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <img  alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} />
       <div className="content">
         <div className="header">
           {video.snippet.title}
