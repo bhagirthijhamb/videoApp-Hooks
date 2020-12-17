a536ef0158ef3a863fcb31284d84ada57344548e
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-17 12:26:28 -0500

    created Video Detail component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from './../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-  state = { videos: [] }
+  state = { videos: [], selectedVideo: null }
+
+  // callback function defined as arrow function
   onTermSubmit = async (term) => {
     console.log(term)
     // youtube is now a preconfigured instance of axios
@@ -15,11 +19,18 @@ class App extends React.Component {
     console.log(response.data.items)
     this.setState({ videos: response.data.items })
   }
+
+  // callback function defined as arrow function
+  onVideoSelect = (video) => {
+    // console.log('From the App', video);
+    this.setState({ selectedVideo: video })
+  }
   render(){
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        i have {this.state.videos.length} videos
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
       </div>
     )
   }
