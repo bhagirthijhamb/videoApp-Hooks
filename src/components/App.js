1f82e4af7ec3c9b2915187c9ca82c7c82afdd90b
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-22 16:24:20 -0500

    created the custom hook - useVideos

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import youtube from './../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null }
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  componentDidMount(){
-    this.onTermSubmit('plants');
-  }
+  useEffect(() => {
+    onTermSubmit('plants')
+  }, [])
 
   // callback function defined as arrow function
-  onTermSubmit = async (term) => {
-    console.log(term)
+  const onTermSubmit = async (term) => {
+    // console.log(term)
     // youtube is now a preconfigured instance of axios
     const response = await youtube.get('/search', {
       params: {
         q: term
       }
     })
-    console.log(response.data.items)
-    this.setState({ 
-      videos: response.data.items,
-      selectedVideo: response.data.items[0]
-    })
+    // console.log(response.data.items)
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   }
 
   // callback function defined as arrow function
-  onVideoSelect = (video) => {
-    // console.log('From the App', video);
-    this.setState({ selectedVideo: video })
-  }
-  render(){
-    return (
+  // const onVideoSelect = (video) => {
+  //   // console.log('From the App', video);
+  //   setSelectedVideo(video)
+  // }
+
+  return (
       <div className="ui container">
-        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <SearchBar onTermSubmit={onTermSubmit} />
         <div className="ui grid">
           <div className="ui row">
             <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
+              <VideoDetail video={selectedVideo} />
             </div>
             <div className="five wide column">
-              <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+              <VideoList 
+                videos={videos} 
+                // onVideoSelect={(video) => setSelectedVideo(video)} 
+                onVideoSelect={setSelectedVideo} 
+              />
             </div>
           </div>
         </div>
       </div>
     )
-  }
 }
 
 export default App;
---
+++ src/hooks/useVideos.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import youtube from './../apis/youtube';
+
+const useVideos = (defaultSearchTerm) => {
+  const [videos, setVideos] = useState([]);
+  
+  useEffect(() => {
+    search(defaultSearchTerm)
+  }, [])
+
+  const search = async (term) => {
+    // console.log(term)
+    // youtube is now a preconfigured instance of axios
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    })
+    // console.log(response.data.items)
+    setVideos(response.data.items);
+  }
+  // return { videos, onTermSubmit }
+  return [videos, search];
+}
+
+export default useVideos;
