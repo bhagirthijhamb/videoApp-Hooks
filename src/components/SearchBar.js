c0bc5bcb04e4807e5ec72c4b3593a84af92a40c3
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2020-12-22 14:24:49 -0500

    Refactored Searchbar component to use hooks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,22 @@
-import React from 'react';
+import React, { useState} from 'react';
 
-class SearchBar extends React.Component {
-  state = { term: '' }
-  onInputChange = (e) => {
-    this.setState({ term: e.target.value })
-  }
-  onFormSubmit = (e) => {
+const SearchBar = ({ onTermSubmit }) => {
+  const [term, setTerm] = useState('');
+
+  const onFormSubmit = (e) => {
     e.preventDefault();
-    // TODO: Make sure we call callback from parent component
-    this.props.onTermSubmit(this.state.term)
+    onTermSubmit(term)
   }
-  render(){
-    return (
+  return (
       <div className="search-bar ui segment">
-        <form className="ui form" onSubmit={this.onFormSubmit}>
+        <form className="ui form" onSubmit={onFormSubmit}>
           <div className="field">
             <label>Video Search</label>
-            <input type="text" value={this.state.term} onChange={this.onInputChange} />
+            <input type="text" value={term} onChange={(e) => setTerm(e.target.value)} />
           </div>
         </form>
       </div>
     )
-  }
 }
 
 export default SearchBar;
